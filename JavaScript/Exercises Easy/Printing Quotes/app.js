4924e0fec2d168fa338191dc1b818017980a8796
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2024-09-30 00:40:06 +0200

    docs: start drafting some ideas for the game

--- JavaScript/Exercises Easy/Printing Quotes/app.js
+++ JavaScript/Exercises Easy/Printing Quotes/app.js
@@ -1,5 +1,5 @@
-let input = prompt("Whats the quuote?");
-let author = prompt("Who said it?");
-let output = author + " " + "says," + " " + '"' + input + '"';
+let input = prompt('Whats the quuote?');
+let author = prompt('Who said it?');
+let output = author + " " + "says\," + " " + "\""+input+"\"";
 
-alert(output);
+alert(output)
