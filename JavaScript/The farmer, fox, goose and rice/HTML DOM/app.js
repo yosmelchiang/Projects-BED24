69747ae1f963ea98b45b72ec253dfa8492f28dfd
Yosmel Chiang <97784146+yosmelchiang@users.noreply.github.com>
2024-10-01 21:49:34 +0200

    fix: add variables for DOM elements to clean up the code

--- JavaScript/The farmer, fox, goose and rice/HTML DOM/app.js
+++ JavaScript/The farmer, fox, goose and rice/HTML DOM/app.js
@@ -1,262 +1,238 @@
-// We are going to initialize two arrays for moving the entities
-// We need a way to move the entities from one array to the other
-
-//Array.filter()
-// Creates a copy of an Array, iterating each item by the filter we provide, uses a function to do the filtering
-let log = console.log; //For testing purposes
-
-let sShore = ["Farmer", "Fox", "Goose", "Corn"];
-let nShore = [];
-
-// HTML Handler
-display("state");
+// Functions
 
 function move(resource) {
-  if (resource == "Goose") {
+  if (resource == 'Goose') {
     // Illegal move
-    if (nShore.includes(resource) && !nShore.includes("Farmer")) {
-      display("illegalMove");
+    if (nShore.includes(resource) && !nShore.includes('Farmer')) {
+      display('illegalMove');
       return; //Prevents the rest of the code on executing if an illegal move is reached
-    } else if (sShore.includes(resource) && !sShore.includes("Farmer")) {
-      display("illegalMove");
+    } else if (sShore.includes(resource) && !sShore.includes('Farmer')) {
+      display('illegalMove');
       return;
     }
 
     if (sShore.includes(resource)) {
-      sShore = sShore.filter(
-        (items) => items !== resource && items !== "Farmer"
-      ); // We are going through each element in sShore and filtering the array by every element that is not Goose and Farmer
+      sShore = sShore.filter((items) => items !== resource && items !== 'Farmer'); // We are going through each element in sShore and filtering the array by every element that is not Goose and Farmer
       nShore.push(resource);
 
       //Checks if the farmer is already in that shore, if not, push
-      if (nShore.includes("Farmer")) {
+      if (nShore.includes('Farmer')) {
       } else {
-        nShore.push("Farmer");
+        nShore.push('Farmer');
       }
 
       checkSafety();
-      display("state");
+      display('state');
     } else {
-      nShore = nShore.filter(
-        (items) => items !== resource && items != "Farmer"
-      );
-      sShore.push(resource, "Farmer");
+      nShore = nShore.filter((items) => items !== resource && items != 'Farmer');
+      sShore.push(resource, 'Farmer');
 
       //Checks if the farmer is already in that shore, if not, push
-      if (sShore.includes("Farmer")) {
+      if (sShore.includes('Farmer')) {
       } else {
-        sShore.push("Farmer");
+        sShore.push('Farmer');
       }
 
       checkSafety();
-      display("state");
+      display('state');
     }
   }
 
-  if (resource == "Fox") {
+  if (resource == 'Fox') {
     // Illegal move
-    if (nShore.includes(resource) && !nShore.includes("Farmer")) {
-      display("illegalMove");
+    if (nShore.includes(resource) && !nShore.includes('Farmer')) {
+      display('illegalMove');
       return; //Prevents the rest of the code on executing if an illegal move is reached
-    } else if (sShore.includes(resource) && !sShore.includes("Farmer")) {
-      display("illegalMove");
+    } else if (sShore.includes(resource) && !sShore.includes('Farmer')) {
+      display('illegalMove');
       return;
     }
 
     if (sShore.includes(resource)) {
-      sShore = sShore.filter(
-        (items) => items !== resource && items !== "Farmer"
-      );
+      sShore = sShore.filter((items) => items !== resource && items !== 'Farmer');
       nShore.push(resource);
 
       //Checks if the farmer is already in that shore, if not, push
-      if (nShore.includes("Farmer")) {
+      if (nShore.includes('Farmer')) {
       } else {
-        nShore.push("Farmer");
+        nShore.push('Farmer');
       }
 
       checkSafety();
-      display("state");
+      display('state');
     } else {
-      nShore = nShore.filter(
-        (items) => items !== resource && items != "Farmer"
-      );
+      nShore = nShore.filter((items) => items !== resource && items != 'Farmer');
       sShore.push(resource);
 
       //Checks if the farmer is already in that shore, if not, push
-      if (sShore.includes("Farmer")) {
+      if (sShore.includes('Farmer')) {
       } else {
-        sShore.push("Farmer");
+        sShore.push('Farmer');
       }
 
       checkSafety();
-      display("state");
+      display('state');
     }
   }
 
-  if (resource == "Corn") {
+  if (resource == 'Corn') {
     // Illegal move
-    if (nShore.includes(resource) && !nShore.includes("Farmer")) {
-      display("illegalMove");
+    if (nShore.includes(resource) && !nShore.includes('Farmer')) {
+      display('illegalMove');
       return; //Prevents the rest of the code on executing if an illegal move is reached
-    } else if (sShore.includes(resource) && !sShore.includes("Farmer")) {
-      display("illegalMove");
+    } else if (sShore.includes(resource) && !sShore.includes('Farmer')) {
+      display('illegalMove');
       return;
     }
 
     if (sShore.includes(resource)) {
-      sShore = sShore.filter(
-        (items) => items !== resource && items !== "Farmer"
-      );
+      sShore = sShore.filter((items) => items !== resource && items !== 'Farmer');
       nShore.push(resource);
 
       //Checks if the farmer is already in that shore, if not, push
-      if (nShore.includes("Farmer")) {
+      if (nShore.includes('Farmer')) {
       } else {
-        nShore.push("Farmer");
+        nShore.push('Farmer');
       }
 
       checkSafety();
-      display("state");
+      display('state');
     } else {
-      nShore = nShore.filter(
-        (items) => items !== resource && items != "Farmer"
-      );
+      nShore = nShore.filter((items) => items !== resource && items != 'Farmer');
       sShore.push(resource);
 
       //Checks if the farmer is already in that shore, if not, push
-      if (sShore.includes("Farmer")) {
+      if (sShore.includes('Farmer')) {
       } else {
-        sShore.push("Farmer");
+        sShore.push('Farmer');
       }
 
       checkSafety();
-      display("state");
+      display('state');
     }
   }
 
-  if (resource == "Farmer") {
+  if (resource == 'Farmer') {
     if (sShore.includes(resource)) {
       sShore = sShore.filter((items) => items !== resource);
 
       //Checks if the farmer is already in that shore, if not, push
-      if (nShore.includes("Farmer")) {
+      if (nShore.includes('Farmer')) {
       } else {
-        nShore.push("Farmer");
+        nShore.push('Farmer');
       }
 
       checkSafety();
-      display("state");
+      display('state');
     } else {
       nShore = nShore.filter((items) => items !== resource);
       //Checks if the farmer is already in that shore, if not, push
-      if (sShore.includes("Farmer")) {
+      if (sShore.includes('Farmer')) {
       } else {
-        sShore.push("Farmer");
+        sShore.push('Farmer');
       }
 
       checkSafety();
-      display("state");
+      display('state');
     }
   }
 }
 
 function display(type) {
-  if (type == "state") {
-    document.getElementById("southShore").innerHTML = sShore.join(", ");
-    document.getElementById("northShore").innerHTML = nShore.join(", ");
-  } else if (type == "gooseAteCorn") {
+  if (type == 'state') {
+    domsShoreEle.innerHTML = sShore.join(', ');
+    domnShoreEle.innerHTML = nShore.join(', ');
+  } else if (type == 'gooseAteCorn') {
     disableControls();
-    document.getElementById("message").innerHTML = "🪿The Goose ate the Corn 😭";
-    document.getElementById("message").style.color = "white";
-    document.getElementById("message").style.backgroundColor = "red";
+    domMessageEle.innerHTML = gooseAteCorn;
+    domMessageEle.style.color = 'white';
+    domMessageEle.style.backgroundColor = 'red';
     // Clears the message after 3 seconds
     setTimeout(() => {
-      document.getElementById("message").innerHTML = "";
+      domMessageEle.innerHTML = '';
       restart();
     }, 3000);
-  } else if (type == "foxAteGoose") {
+  } else if (type == 'foxAteGoose') {
     disableControls();
-    document.getElementById("message").innerHTML =
-      "🦊 The Fox ate the Goose 😭";
-    document.getElementById("message").style.color = "white";
-    document.getElementById("message").style.backgroundColor = "red";
+    domMessageEle.innerHTML = foxAteGoose;
+    domMessageEle.style.color = 'white';
+    domMessageEle.style.backgroundColor = 'red';
     // Clears the message after 3 seconds
     setTimeout(() => {
-      document.getElementById("message").innerHTML = "";
+      domMessageEle.innerHTML = '';
       restart();
     }, 3000);
-  } else if (type == "allSafe") {
-    //Disabled this because it was annoying.
-    document.getElementById("message").innerHTML =
-      "👩‍🌾 Transportation successful, no animals were harmed during this move";
-    document.getElementById("message").style.color = "white";
-    document.getElementById("message").style.backgroundColor = "green";
-    // Clears the message after 4 seconds
-    setTimeout(() => {
-      document.getElementById("message").innerHTML = "";
-    }, 3000);
-  } else if (type == "illegalMove") {
-    document.getElementById("message").innerHTML =
-      "You are trying to move this animal without the presence of the farmer, maybe try moving the farmer to that Shore first?";
-    document.getElementById("message").style.color = "white";
-    document.getElementById("message").style.backgroundColor = "red";
+  } else if (type == 'illegalMove') {
+    domMessageEle.innerHTML = illegalMove;
+    domMessageEle.style.color = 'white';
+    domMessageEle.style.backgroundColor = 'red';
     // Clears the message after 3 seconds
     setTimeout(() => {
-      document.getElementById("message").innerHTML = "";
+      domMessageEle.innerHTML = '';
     }, 3000);
-  } else if (type == "allMoved") {
-    document.getElementById("message").innerHTML =
-      "Everyone is on the North Shore. Good job!";
-    document.getElementById("message").style.color = "white";
-    document.getElementById("message").style.backgroundColor = "green";
+  } else if (type == 'allMoved') {
+    domMessageEle.innerHTML = allMoved;
+    domMessageEle.style.color = 'white';
+    domMessageEle.style.backgroundColor = 'green';
   }
 }
 
 function checkSafety() {
-  if (
-    sShore.includes("Fox") &&
-    sShore.includes("Goose") &&
-    !sShore.includes("Farmer")
-  ) {
-    display("foxAteGoose");
-  } else if (
-    nShore.includes("Fox") &&
-    nShore.includes("Goose") &&
-    !nShore.includes("Farmer")
-  ) {
-    display("foxAteGoose");
-  } else if (
-    sShore.includes("Goose") &&
-    sShore.includes("Corn") &&
-    !sShore.includes("Farmer")
-  ) {
-    display("gooseAteCorn");
-  } else if (
-    nShore.includes("Goose") &&
-    nShore.includes("Corn") &&
-    !nShore.includes("Farmer")
-  ) {
-    display("gooseAteCorn");
+  if (sShore.includes('Fox') && sShore.includes('Goose') && !sShore.includes('Farmer')) {
+    display('foxAteGoose');
+  } else if (nShore.includes('Fox') && nShore.includes('Goose') && !nShore.includes('Farmer')) {
+    display('foxAteGoose');
+  } else if (sShore.includes('Goose') && sShore.includes('Corn') && !sShore.includes('Farmer')) {
+    display('gooseAteCorn');
+  } else if (nShore.includes('Goose') && nShore.includes('Corn') && !nShore.includes('Farmer')) {
+    display('gooseAteCorn');
   } else if (nShore.length === 4) {
-    display("allMoved");
+    display('allMoved');
   } else {
   }
 }
 
 function disableControls() {
-  document.getElementById("farmerBtn").style.display = "none";
-  document.getElementById("gooseBtn").style.display = "none";
-  document.getElementById("cornBtn").style.display = "none";
-  document.getElementById("foxBtn").style.display = "none";
+  farmerButton.style.display = 'none';
+  gooseButton.style.display = 'none';
+  cornButton.style.display = 'none';
+  foxButton.style.display = 'none';
 }
 
 function restart() {
-  sShore = ["Farmer", "Fox", "Goose", "Corn"];
+  sShore = ['Farmer', 'Fox', 'Goose', 'Corn'];
   nShore = [];
-  display("state");
-  document.getElementById("farmerBtn").style.display = "inline";
-  document.getElementById("gooseBtn").style.display = "inline";
-  document.getElementById("cornBtn").style.display = "inline";
-  document.getElementById("foxBtn").style.display = "inline";
+  display('state');
+  farmerButton.style.display = 'inline';
+  gooseButton.style.display = 'inline';
+  cornButton.style.display = 'inline';
+  foxButton.style.display = 'inline';
 }
+
+// Console
+const log = console.log; //For testing purposes
+
+// Arrays
+let sShore = ['Farmer', 'Fox', 'Goose', 'Corn'];
+let nShore = [];
+
+// DOMelements
+const domMessageEle = document.getElementById('message');
+const domsShoreEle = document.getElementById('southShore');
+const domnShoreEle = document.getElementById('northShore');
+
+// HTML Display Message Handler
+display('state');
+
+// HTML Display Message
+const gooseAteCorn = '🪿The Goose ate the Corn 😭';
+const foxAteGoose = '🦊 The Fox ate the Goose 😭';
+const illegalMove =
+  'You are trying to move this animal without the presence of the farmer, maybe try moving the farmer to that Shore first?';
+const allMoved = 'Everyone is on the North Shore. Good job!';
+
+// HTML Buttons
+const farmerButton = document.getElementById('farmerBtn');
+const gooseButton = document.getElementById('gooseBtn');
+const cornButton = document.getElementById('cornBtn');
+const foxButton = document.getElementById('foxBtn');
